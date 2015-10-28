948d2f6fd333520aaf4be49ba01aa90dc7bbfa0b
Martin Aranda <martinaranda@al.uap.edu.ar>
2015-10-28 15:18:13 -0300

    Capacidad de modificar el email de los usuarios

--- vistas/users/edit.php
+++ vistas/users/edit.php
@@ -10,6 +10,10 @@
 		<label for="password">Contraseña: </label>
 		<input type="password" class="form-control" name="user[password]" id="password">
 	</div>
+	<div class="form-group">
+		<label for="name">Email (opcional): </label>
+		<input type="email" class="form-control" name="user[email]" id="name">
+	</div>
 	<div class="text-center">
 		<button type="submit" class="btn btn-success">Guardar</button>
 		<a href="/admin/usuarios" class="btn btn-primary">Volver</a>
